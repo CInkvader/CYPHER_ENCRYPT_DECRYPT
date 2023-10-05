2a94bb905c58162b2d61fb39e3247b819991a248
chrisartmlorilla@gmail.com <chrisartmlorilla@gmail.com>
2023-10-05 15:22:14 +0800

    Updated code

--- PRELIM/Program.cs
+++ PRELIM/Program.cs
@@ -1,80 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Data;
 using System.IO;
 
 namespace PRELIM
 {
     internal class Program
     {
-        List<char> EncryptKey = new List<char>();
+        static List<char> EncryptKey = new List<char>();
+        static string filename = "eMessage.txt";
 
-        private void run()
+        private static void Run()
         {
+            char mode;
+
+            mode = SetMachineMode();
+            Console.WriteLine("Machine Mode has been set.");
+            Console.ReadKey();
+            Console.Clear();
+
+            EnterKey();
+            if (mode == 'E')
+            {
+                EncryptMode();
+                return;
+            }
+            DecryptMode();
+        }
+
+        private static char SetMachineMode()
+        {
+            string input;
             do
             {
                 Console.Write("Would you like to encrypt or decrypt a message? [E / D] : ");
-                string input = Console.ReadLine();
-                char choice = ' ';
 
-                if (!(input == string.Empty))
-                {
-                    choice = char.ToUpper(input[0]);
-                }
-                
-                if (choice != 'E' && choice != 'D')
+                input = Console.ReadLine();
+                if (input.Length != 1 || (char.ToUpper(input[0]) != 'E' && char.ToUpper(input[0]) != 'D'))
                 {
                     Console.WriteLine("Invalid Setting please try again. Press any key to continue.");
                     Console.ReadKey();
                     Console.Clear();
                     continue;
                 }
-
-                Console.WriteLine("Machine Mode has been set.");
-                Console.ReadKey();
-                Console.Clear();
-
-                if (choice == 'E')
-                {
-                    encryptMode();
-                    break;
-                }
-                if (choice == 'D')
-                {
-                    decryptMode();
-                    break;
-                }
+                break;
             } while (true);
-            Console.WriteLine("Press any key to close the program");
-            Console.ReadKey();
+
+            return char.ToUpper(input[0]);
         }
 
-        private void encryptMode()
-        {
-            EnterKey();
-
-            Console.WriteLine("Please input the message you want to encrypt:");
-            string message = Console.ReadLine();
-
-            encryptMessage(message);
-        }
-
-        private void EnterKey()
+        private static void EnterKey()
         {
             Console.Write("What is the key you want to set? : ");
             string key = Console.ReadLine();
-            setKey(key.ToUpper());
+            SetKey(key.ToUpper());
 
             Console.WriteLine("Cypher has been set.");
             Console.ReadKey();
             Console.Clear();
         }
 
-        private void setKey(string key)
+        private static void SetKey(string key)
         {
             for (int i = 0; i < key.Length; i++)
             {
@@ -94,54 +79,45 @@ namespace PRELIM
             }
         }
 
-        private void encryptMessage(string message)
+        private static void EncryptMode()
+        {
+            Console.WriteLine("Please input the message you want to encrypt:");
+            string message = Console.ReadLine();
+
+            WriteFile(EncryptMessage(message.ToUpper()));
+            Console.WriteLine("Message has been successfully encrypted and written to eMessage.txt");
+        }
+
+        private static string EncryptMessage(string message)
         {
             string newMessage = string.Empty;
-            char letter;
-
-            int univIndex = 0;
 
             for (int i = 0; i < message.Length; i++)
             {
-                letter = Char.ToUpper(message[i]);
-
-                if (!(letter >= 65 && letter <= 90))
+                if (!(message[i] >= 65 && message[i] <= 90))
                 {
-                    newMessage += letter;
+                    newMessage += message[i];
+                    continue;
                 }
-                if (letter - 65 < EncryptKey.Count)
-                {
-                    int index = (letter - 65);
-                    newMessage += (char)EncryptKey[index];
-                }
-                if (letter - 65 >= EncryptKey.Count)
-                {
-                    char newChar = (char)(letter - EncryptKey.Count - univIndex);
-
-                    while (EncryptKey.Contains(newChar))
-                    {
-                        univIndex--;
-                        newChar++;
-                    }
-                    newMessage += newChar;
-                }
+                newMessage += EncryptKey[message[i] - 65];
             }
-            writeFile(newMessage);
+            return newMessage;
         }
 
-        private void decryptMode()
+        private static void DecryptMode()
         {
-            EnterKey();
-            
+            string decryptedMessage;
+
             Console.WriteLine("Reading eMessage.txt and decrypting using the provided key.\nThe decrypted message is:");
-            string decryptedMessage = decryptMessage(readFile());
+            decryptedMessage = DecryptMessage(ReadFile());
 
             Console.WriteLine($"{decryptedMessage}\nMessage has been successfully decrypted.");
         }
 
-        private string decryptMessage(string message)
+        private static string DecryptMessage(string message)
         {
             string newMessage = string.Empty;
+
             for (int i = 0; i < message.Length; i++)
             {
                 if (EncryptKey.Contains(message[i]))
@@ -149,32 +125,24 @@ namespace PRELIM
                     newMessage += (char)(EncryptKey.IndexOf(message[i]) + 65);
                     continue;
                 }
-                else
-                {
-                    newMessage += message[i];
-                }
+                newMessage += message[i];
             }
             return newMessage;
         }
 
-        private void writeFile(string message)
+        private static void WriteFile(string message)
         {
-            string filename = "eMessage.txt";
-            File.WriteAllText(filename, message);
-
-            Console.WriteLine("Message has been successfully encrypted and written to eMessage.txt");
+            File.WriteAllText(filename, message);            
         }
 
-        private string readFile()
+        private static string ReadFile()
         {
-            string filename = "eMessage.txt";
             return File.ReadAllText(filename);
         }
 
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.run();
+            Run();
         }
     }
 }
